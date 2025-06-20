6610f83fa8927cc9f28b2b9cf61d729cd4d52176
selfcan <99958756+selfcan@users.noreply.github.com>
2025-06-21 07:29:52 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -69,7 +69,7 @@ UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 UPDATE_PACKAGE "luci-app-adguardhome" "sirpdboy/luci-app-adguardhome" "js"
-UPDATE_PACKAGE "luci-app-parentcontrol" "sirpdboy/luci-app-parentcontrol" "main" "pkg"
+UPDATE_PACKAGE "luci-app-parentcontrol" "sirpdboy/luci-app-parentcontrol" "main"
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
